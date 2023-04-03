b6f1b3cc0015d3e16033aa02de2c73ccbd11d67e
shems95 <61524292+shems95@users.noreply.github.com>
2023-04-04 01:08:44 +0200

    Aggiunta Dockerfile

--- src/main/java/com/example/springmaven/SpringMavenApplication.java
+++ src/main/java/com/example/springmaven/SpringMavenApplication.java
@@ -2,11 +2,14 @@ package com.example.springmaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringMavenApplication {
-
     public static void main(String[] args) {
+        System.out.println("YEEEESSS");
         SpringApplication.run(SpringMavenApplication.class, args);
     }
 
